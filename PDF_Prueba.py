53fc5f4e3fa730aec0b055cd73fa22ea67b28e41
Vyanei Salazar <manchas_0207@hotmail.com>
2022-12-16 17:32:22 -0600

    Codigo para email

--- PDF_Prueba.py
+++ PDF_Prueba.py
@@ -10,6 +10,14 @@ from reportlab.pdfgen import canvas
 # Libreria para importar a base64
 from io import open
 import base64
+# Librerias para emails
+import getpass
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 PAGE_HEIGHT = defaultPageSize[1]
 PAGE_WIDTH = defaultPageSize[0]
@@ -38,6 +46,7 @@ t = Table(
 story.append(t)
 doc.build(story)
 
+"""
 #  Creando archivo de texto en base64
 archivo_b64 = open("Prueba.txt", "w")
 
@@ -46,3 +55,40 @@ with open("test.pdf", "rb") as pdf_file:
 
 archivo_b64.write(str(encoded_string))
 archivo_b64.close()
+"""
+
+# ****************EN FASE DE PRUEBA, NO USAR ESTA PARTE AUN***********************
+
+# Enviando email con PDF
+time.sleep(1)
+remitente = input("Ingrese el correo del remitente: ")
+pssword = getpass.getpass("Ingrese su contraseña: ")
+destinatarios = input("Ingrese el/los destinatarios: ")
+asunto = input("Ingrese el asunto: ")
+cuerpo = input("Ingrese el mensaje del correo: ")
+ruta_adjunto = input("Ingrese la ruta del archivo: ")
+nombre_adjunto = input("Ingrese el nombre del archivo: ")
+
+mensaje = MIMEMultipart()
+mensaje["From"] = remitente
+mensaje["To"] = destinatarios
+mensaje["Subject"] = asunto
+
+mensaje.attach(MIMEText(cuerpo,"plain"))
+
+archivo_adjunto = open(ruta_adjunto, "rb")
+
+adjunto_MIME = MIMEBase("application", "octect-stream")
+adjunto_MIME.set_payload((archivo_adjunto).read())
+
+encoders.encode_base64(adjunto_MIME)
+
+adjunto_MIME.add_header("Content-Disposition", "attachment; filename= %s" % nombre_adjunto)
+mensaje.attach(adjunto_MIME)
+
+sesion_smtp = smtplib.SMTP("stmp.gmail.com", 587)
+sesion_smtp.starttls()  # Cifrando conexion
+sesion_smtp.login(remitente,pssword)
+texto = mensaje.as_string()
+sesion_smtp.sendmail(remitente,destinatarios,texto)
+sesion_smtp.quit()
